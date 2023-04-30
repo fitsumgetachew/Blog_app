2c63996a525703dd993e057c5ae45d0a96061809
fitsumgetachew <fitsumgeta2021@gmail.com>
2023-04-30 12:48:24 +0000

    routing with 2 url

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    #running with debug mode on by giving some error by ading  --debug
-    return "<h1>Running with debug on </h1>" + name
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello world'
